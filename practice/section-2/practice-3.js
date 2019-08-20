4a4516d0f228666ff28d59ea72c9af03aa21921c
wanghc00 <306093226@qq.com>
2019-08-20 09:12:52 +0800

    作业提交

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -5,46 +5,19 @@ function countSameElements(collection) {
 	var myObject={};
 	var findObj=0;
 	for(var i=0;i<collection.length;i++){
-    if(collection[i].indexOf("-") != -1){
-      var arr=collection[i].split("-");
+    if(collection[i].indexOf("-") != -1||collection[i].indexOf("[")!= -1||collection[i].indexOf(":") != -1){
+      var arr0 = collection[i].replace(/[^a-z]+/ig,"");
+      var arr1 = collection[i].replace(/[^0-9]/ig, "");
       for(var key in myObject){
         findObj=0;
-        if(key==arr[0]){
-          myObject[key]+=parseInt(arr[1]);
+        if(key==arr0){
+          myObject[key]+=parseInt(arr1);
           findObj=1;
           break;
         }
       }
       if(findObj==0){
-        myObject[arr[0]]=parseInt(arr[1]);
-      }
-    }else if(collection[i].indexOf("[")!= -1){
-      var arr=collection[i].split("[");
-      for(var key in myObject){
-        findObj=0;
-        if(key==arr[0]){
-          var num=collection[i].replace(/[^0-9]/ig, "");
-          myObject[key]+=parseInt(num);
-          findObj=1;
-          break;
-        }
-      }
-      if(findObj==0){
-        var num=collection[i].replace(/[^0-9]/ig, "");
-        myObject[arr[0]]=parseInt(num);
-      }
-    }else if(collection[i].indexOf(":") != -1){
-      var arr=collection[i].split(":");
-      for(var key in myObject){
-        findObj=0;
-        if(key==arr[0]){
-          myObject[key]+=parseInt(arr[1]);
-          findObj=1;
-          break;
-        }
-      }
-      if(findObj==0){
-        myObject[arr[0]]=parseInt(arr[1]);
+        myObject[arr0]=parseInt(arr1);
       }
     }else{
       for(var key in myObject){
