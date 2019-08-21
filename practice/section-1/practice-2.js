a2ec741ac4b3cba71d47a50ac2fe459c09790c7a
wanghc00 <306093226@qq.com>
2019-08-21 10:39:29 +0800

    practice1.2

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -2,14 +2,13 @@
 
 function collectSameElements(collectionA, collectionB) {
   var myArray=new Array();
-	for(var i=0;i<collectionA.length;i++){
-		for(var j=0;j<collectionB[0].length;j++){
-			for(var m=0;m<collectionB.length;m++){
-				if(collectionA[i]==collectionB[m][j]){
-					myArray.push(collectionA[i]);
-				}
-			}
-		}
-	}
+  for(var i=0;i<collectionA.length;i++){
+	  for(var j=0;j<collectionB.length;j++){
+		  for(var m=0;m<collectionB[j].length;m++){
+			  if(collectionA[i]===collectionB[j][m])
+			  myArray.push(collectionA[i]);
+		  }
+	  }
+  }
 	return myArray;
 }
